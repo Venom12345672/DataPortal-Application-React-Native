3ef113880ec06098385d238ad648ddcdc9933687
Venom12345672 <48963915+Venom12345672@users.noreply.github.com>
2019-07-30 10:49:40 +0500

    starting mainMenu

--- src/components/CategoryPage.js
+++ src/components/CategoryPage.js
@@ -1,8 +1,37 @@
 import React from "react";
-import { Text, View, StyleSheet, Dimensions, Image } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
+import { SearchBar } from 'react-native-elements';
 
 export default class MainScreen extends React.Component {
+  state = {
+    search: ""
+  };
+
+  updateSearch = search => {
+    this.setState({ search });
+  };
+
   render() {
-    return <Text>Category Page</Text>;
+    const { search } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <SearchBar
+          placeholder="Type Here.."
+          onChangeText={this.updateSearch}
+          value={search}
+        />
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 15,
+    //backgroundColor: "#fff",
+    //alignItems: "center",
+    //justifyContent: "flex-start"
+  }
+});
