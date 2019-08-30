b66788fd922c494e8941eadfd7525b56854263b4
Venom12345672 <48963915+Venom12345672@users.noreply.github.com>
2019-08-31 03:07:18 +0500

    audio generator\

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -8,21 +8,34 @@ import {
   ImageBackground,
   Dimensions,
   TouchableHighlight,
-  TouchableOpacity
+  TouchableOpacity,
+  ToastAndroid
 } from "react-native";
 
 const phone_width = Dimensions.get("window").width;
 import Settings from "./settings.json";
-
 import Carousel from "react-native-carousel-view";
 import ToggleSwitch from "toggle-switch-react-native";
 import { sectionStyles } from "./SectionStyling";
 import Icon from "react-native-vector-icons/Foundation";
 import ContentControlPanel from "./ContentControlPanel";
+import Sound from "react-native-sound";
+
 export default class Section1 extends React.Component {
+  sound = [
+    new Sound("tone_1.mp3"),
+    new Sound("tone_2.mp3"),
+    new Sound("tone_3.mp3"),
+    new Sound("tone_4.mp3")
+  ];
+
+  // playSound = () => {
+  //   this.sound.play();
+  // };
+
   state = {
     nightMode: false,
-    longPress: [false,false,false,false],
+    longPress: [false, false, false, false],
     lineHeight: Settings.currentLineHeightEnglish,
     descriptionFont: Settings.currentSettingsEnglish[0],
     contentFont: Settings.currentSettingsEnglish[1],
@@ -72,8 +85,17 @@ export default class Section1 extends React.Component {
     this.setState({ nightMode: !this.state.nightMode });
     this.props.navigation.setParams({ nightMode: !this.state.nightMode });
   };
-  _onLongPress = () => {
-    alert("hamzah")
+  _onLongPress = index => {
+    let temp = this.state.longPress;
+    if (this.state.longPress[index] == true) {
+      temp[index] = false;
+      this.sound[index].stop()
+    } else {
+      temp = this.state.longPress.map((_, i) => i == index);
+      this.sound[index].play();
+    }
+    this.setState({ longPress: temp });
+    
   };
   refreshContent = () => {
     this.setState({
@@ -113,7 +135,7 @@ export default class Section1 extends React.Component {
               >
                 Signs in Autistic Children
               </Text>
-              <TouchableOpacity key={0} onLongPress={this._onLongPress}>
+              <TouchableOpacity onLongPress={() => this._onLongPress(0)}>
                 <Text
                   style={[
                     this.state.nightMode
@@ -126,11 +148,12 @@ export default class Section1 extends React.Component {
                   ]}
                 >
                   {this.highlight(
-                    "Autsim, or autsim spectrum disorder (ASD), refers to a broad range of conditions characterized by challenges with social skills, repetitive behaviors, speech and nonverbal communcitaion. According to the Centers for Disease Control, autism affects an estimated 1 in 59 children in United States today."
-                  ,0)}
+                    "Autsim, or autsim spectrum disorder (ASD), refers to a broad range of conditions characterized by challenges with social skills, repetitive behaviors, speech and nonverbal communcitaion. According to the Centers for Disease Control, autism affects an estimated 1 in 59 children in United States today.",
+                    0
+                  )}
                 </Text>
               </TouchableOpacity>
-              <TouchableOpacity onLongPress={this._onLongPress}>
+              <TouchableOpacity onLongPress={() => this._onLongPress(1)}>
                 <Text
                   style={[
                     this.state.nightMode
@@ -143,8 +166,9 @@ export default class Section1 extends React.Component {
                   ]}
                 >
                   {this.highlight(
-                    "We know that there is not one autism but many subtypes, most influenced by a combinatiopn of genetic and environmental factors. Because autism is a spectrum disorder, each person with autism has a distinct set of strenghts and challenges. The ways in which people with autism learn, thiink and problem-solve can range from highly killed to severely challenged. Some people with ASD may require significiant support in thier daily lives, while others may need less support and, in some cases, live entirely independently.\n"
-                  ,1)}
+                    "We know that there is not one autism but many subtypes, most influenced by a combinatiopn of genetic and environmental factors. Because autism is a spectrum disorder, each person with autism has a distinct set of strenghts and challenges. The ways in which people with autism learn, thiink and problem-solve can range from highly killed to severely challenged. Some people with ASD may require significiant support in thier daily lives, while others may need less support and, in some cases, live entirely independently.\n",
+                    1
+                  )}
                 </Text>
               </TouchableOpacity>
 
@@ -171,7 +195,7 @@ export default class Section1 extends React.Component {
                 "If you've met one person with autism, you've met one person
                 with autism"
               </Text>
-              <TouchableOpacity onLongPress={this._onLongPress}>
+              <TouchableOpacity onLongPress={() => this._onLongPress(2)}>
                 <Text
                   style={[
                     this.state.nightMode
@@ -184,11 +208,12 @@ export default class Section1 extends React.Component {
                   ]}
                 >
                   {this.highlight(
-                    "Autsim, or autsim spectrum disorder (ASD), refers to a broad range of conditions characterized by challenges with social skills, repetitive behaviors, speech and nonverbal communcitaion. According to the Centers for Disease Control, autism affects an estimated 1 in 59 children in United States today."
-                  ,2)}
+                    "Autsim, or autsim spectrum disorder (ASD), refers to a broad range of conditions characterized by challenges with social skills, repetitive behaviors, speech and nonverbal communcitaion. According to the Centers for Disease Control, autism affects an estimated 1 in 59 children in United States today.",
+                    2
+                  )}
                 </Text>
               </TouchableOpacity>
-              <TouchableOpacity onLongPress={this._onLongPress}>
+              <TouchableOpacity onLongPress={() => this._onLongPress(3)}>
                 <Text
                   style={[
                     this.state.nightMode
@@ -201,8 +226,9 @@ export default class Section1 extends React.Component {
                   ]}
                 >
                   {this.highlight(
-                    "We know that there is not one autism but many subtypes, most influenced by a combinatiopn of genetic and environmental factors. Because autism is a spectrum disorder, each person with autism has a distinct set of strenghts and challenges. The ways in which people with autism learn, thiink and problem-solve can range from highly killed to severely challenged. Some people with ASD may require significiant support in thier daily lives, while others may need less support and, in some cases, live entirely independently.\n\n"
-                  ,3)}
+                    "We know that there is not one autism but many subtypes, most influenced by a combinatiopn of genetic and environmental factors. Because autism is a spectrum disorder, each person with autism has a distinct set of strenghts and challenges. The ways in which people with autism learn, thiink and problem-solve can range from highly killed to severely challenged. Some people with ASD may require significiant support in thier daily lives, while others may need less support and, in some cases, live entirely independently.\n\n",
+                    3
+                  )}
                 </Text>
               </TouchableOpacity>
             </Fragment>
